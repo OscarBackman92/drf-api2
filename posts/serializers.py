e56d95dc0ee7743cfafb72fb41ca6b2b65da1e8a
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-10-07 12:52:30 +0200

    post/profile

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,12 +1,14 @@
 
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
     
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -20,7 +22,16 @@ class PostSerializer(serializers.ModelSerializer):
                     'image height is larger than 4096px!')
             
             return value
-    
+        
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+                ).first()
+            return like.id if like else None
+        return None
+        
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -30,5 +41,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter'
+            'title', 'content', 'image', 'image_filter', 'like_id',
         ]
