f559863cc1852abec0e6ceab2364a39e53a19431
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-10-07 07:48:16 +0200

    follwers

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from followers.models import Follower
+from followers.serializers import FollowerSerializer
 
-# Create your views here.
+class FollowersList(generics.ListCreateAPIView):
+    """
+    List followers or create a follower if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+class FollowersDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
