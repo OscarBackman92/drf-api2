4e4172d03b555521b855ae03e131d4449e13b4c1
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-10-07 08:05:34 +0200

    fixoing code

--- followers/urls.py
+++ followers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from followers import views
 
 urlpatterns = [
-    path('followers/', views.FollowersList.as_view()),
-    path('followers/<int:pk>/', views.FollowersDetail.as_view())
+    path('followers/', views.FollowerList.as_view()),
+    path('followers/<int:pk>/', views.FollowerDetail.as_view())
 ]
